1f8ef9d583122d8680b12edbd6054a6065c4e52f
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-05-18 12:02:38 +0100

    task 3

---
+++ userInfo.js
@@ -0,0 +1,16 @@
+
+// TASK THREE(3)
+function createUserProfiles(names, modifiedNames){
+  const createUserProfiles = [];
+  for(let i = 0; i < names.length; i++){
+
+const name = names[i];
+const modifiedName = modifiedNames[i];
+const id = i + 1;
+userProfiles.push({ originalName: name, modifiedName: id});
+  }
+
+
+return userProfiles;
+
+}
